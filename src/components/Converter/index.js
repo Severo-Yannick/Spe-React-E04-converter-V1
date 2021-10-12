e1c9ee00dca6f734b41e515e4c2ac9ccb01bf360
Yannick Severo <severoyannick@gmail.com>
2021-10-12 12:03:58 +0200

    structure de base

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -7,7 +7,21 @@ import './styles.scss';
 // == Composant
 const Converter = () => (
   <div className="converter">
-    <h1>Composant : Converter</h1>
+    <header>
+      <h1>Converter</h1>
+      <p>1 euro</p>
+    </header>
+    <main className="converter__currencies">
+      <ul className="currencies">
+        <li className="currency currencies__title">Currencies</li>
+        <li>Canadian Dollar</li>
+        <li>Swiss Franc</li>
+      </ul>
+    </main>
+    <footer className="converter__footer">
+      <p>1.09</p>
+      <h2>United States Dollar</h2>
+    </footer>
   </div>
 );
 
