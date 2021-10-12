c03a19be52c74e42a4f4b81b02822e415df457e5
Yannick Severo <severoyannick@gmail.com>
2021-10-12 11:42:22 +0200

    nettoyage de la structure

---
+++ src/components/Converter/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const Converter = () => (
+  <div className="converter">
+    <h1>Composant : Converter</h1>
+  </div>
+);
+
+// == Export
+export default Converter;
