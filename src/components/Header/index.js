14fe950507451bae4b79fed784bb15e24a16bbfc
Yannick Severo <severoyannick@gmail.com>
2021-10-13 19:26:21 +0200

    ajout des props au composant Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,24 @@
 // == Import : npm
 import React from 'react';
+import PropTypes from 'prop-types';
 // == Import du fichier scss
 import './styles.scss';
 
-const Header = () => (
+const Header = ({ baseAmout }) => (
   <header className="header">
     <h1 className="header__title">Converter</h1>
-    <p className="header__currency_base">1 euro</p>
+    <p className="header__currency_base">
+      {baseAmout} euro{(baseAmout > 1 ? 's' : '')}
+    </p>
   </header>
 );
+
+Header.PropTypes = {
+  baseAmout: PropTypes.number,
+};
+
+Header.defaultProps = {
+  baseAmout: 1,
+};
 
 export default Header;
