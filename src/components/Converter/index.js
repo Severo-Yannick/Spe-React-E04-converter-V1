aa9e6ae8aa4478c63328144766eb36b429c97bcb
Yannick Severo <severoyannick@gmail.com>
2021-10-14 08:55:34 +0200

    ajout des props au composant Converter

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -1,18 +1,25 @@
 // == Import npm
 import React from 'react';
-
-// == Import
+// == Import : composants
 import Header from 'src/components/Header';
 import Currencies from 'src/components/Currencies';
 import Amount from 'src/components/Amount';
+// == Import : data
+import currenciesData from 'src/data/currencies';
+// == Import : scss
 import './styles.scss';
 
 // == Composant
 const Converter = () => (
   <div className="converter">
     <Header />
-    <Currencies />
-    <Amount />
+    <Currencies
+      currencies={currenciesData}
+    />
+    <Amount
+      name="United state Dollar"
+      value="1.09"
+    />
   </div>
 );
 
