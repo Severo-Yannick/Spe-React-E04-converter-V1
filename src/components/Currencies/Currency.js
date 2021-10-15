d74a12cf0f112544950b25ae1eaa87b781489a18
Yannick Severo <severoyannick@gmail.com>
2021-10-15 08:31:33 +0200

    ajout des props au composant Currency

--- src/components/Currencies/Currency.js
+++ src/components/Currencies/Currency.js
@@ -1,7 +1,13 @@
+// == Import : npm
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Currency = () => (
-  <li className="currency">Australian Dollar</li>
+const Currency = ({ name }) => (
+  <li className="currency">{ name }</li>
 );
+
+Currency.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 
 export default Currency;
