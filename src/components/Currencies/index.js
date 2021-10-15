91a4a19f1bb2160b5233a664c5bcfc7cd06296f4
Yannick Severo <severoyannick@gmail.com>
2021-10-15 08:48:33 +0200

    ajout des props au composant Currencies

--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -1,19 +1,38 @@
-// == Import
+// == Import : npm
 import React from 'react';
+import PropTypes from 'prop-types';
+// == Import : composants
 import Currency from './Currency';
 
+// == Import : scss
 import './styles.scss';
 
-const Currencies = () => (
+const Currencies = ({ currencies }) => (
   <main className="currencies">
     <h2 className="currency currencies__title">Currencies</h2>
     <ul className="currencies__list">
-      <Currency />
-      <Currency />
-      <Currency />
-      <Currency />
+      {currencies.map((currency) => (
+        <Currency
+          key={currency.name}
+          {...currency}
+        />
+      ))}
     </ul>
   </main>
 );
+
+Currencies.prototype = {
+  // la prop currencies reçoit un tableau
+  currencies: PropTypes.arrayOf(
+    // Déversement des objets du tableau grace à la méthode map
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
+Currencies.defaultProps = {
+  currencies: [],
+};
 
 export default Currencies;
