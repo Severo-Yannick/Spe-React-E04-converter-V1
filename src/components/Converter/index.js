4e68605064e49ed5cae33bc225f3f5587d92924c
Yannick Severo <severoyannick@gmail.com>
2021-10-16 21:16:59 +0200

    tranformation du composant fonction Converter en composant classe

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -1,5 +1,5 @@
 // == Import npm
-import React from 'react';
+import React, { Component } from 'react';
 // == Import : composants
 import Header from 'src/components/Header';
 import Currencies from 'src/components/Currencies';
@@ -9,19 +9,41 @@ import currenciesData from 'src/data/currencies';
 // == Import : scss
 import './styles.scss';
 
-// == Composant
-const Converter = () => (
-  <div className="converter">
-    <Header />
-    <Currencies
-      currencies={currenciesData}
-    />
-    <Amount
-      name="United state Dollar"
-      value="1.09"
-    />
-  </div>
-);
+// == Class
+class Converter extends Component {
+  // Eslint impose la création d'un constructor
+  constructor(props) {
+    // super représente le constructeur du parent il attend un paramètre "props"
+    super(props);
+    // ESLint impose la déclaration d'un objet state en tant que variable de classe
+    // il doit obligatoirement s'appeler "this.state"
+    // il est obligatoirement un objet
+    this.state = {
+      open: false,
+    };
+  }
+
+  render() {
+    // destructuring de la clé "open" et création d'une const open
+    const { open } = this.state;
+    return (
+      <div className="converter">
+        <Header />
+        {
+          open && (
+            <Currencies
+              currencies={currenciesData}
+            />
+          )
+        }
+        <Amount
+          name="United State Dollar"
+          value={1.09}
+        />
+      </div>
+    );
+  }
+}
 
 // == Export
 export default Converter;
